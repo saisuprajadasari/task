880bbdf34a1e596101eeda7f79bbcd7f6aa10d6b
saisuprajadasari <45190251+saisuprajadasari@users.noreply.github.com>
2019-01-16 16:37:10 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+Var = input("Enter value")
+print(var)
+
+
+# In[9]:
+
+
+# arithematic operations
+var1 = 10
+var2 = 20
+print(var1 + var2)
+print(var1 - var2)
+print(var1 * var2)
+print(var1 / var2)
+print(var1 % var2)
+print(var1 ** var2)
+print(var2 // var1)
+
+
+# In[14]:
+
+
+#comparison operations
+var1 = 10 
+var2 = 20
+print(var1 == var2)
+print(var1 != var2)
+print(var1 < var2)
+print(var1 > var2)
+print(var1 <= var2)
+print(var1 >= var2)
+
+
+# In[28]:
+
+
+list = [1,2,3]
+print(1 in list)
+4 in list
+#2 not in list
+
+
+# In[39]:
+
+
+#logical 
+var1 = 10
+var2 = 20
+print(var1 and var2)
+print(var1 or var2)
+print( not var2)
+
+
+# In[41]:
+
+
+#identity
+var1 = 10 
+var2 = 20
+print(var1 is var2)
+print(var1 is not var2)
+
+
+# In[57]:
+
+
+import math
+a = 10
+b = 20.12
+print(math.floor(a))
+print(math.floor(b))
+print(math.ceil(a))
+print(math.ceil(b))
+print(math.copysign(a,b))
+print(math.fabs(a))
+print(math.factorial(a))
+
+
+# In[60]:
+
+
+print("\"Hello World\"")
+
+
+# In[ ]:
+
+
+
+
